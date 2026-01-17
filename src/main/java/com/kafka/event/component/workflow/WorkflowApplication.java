9ce7fb939225d12b4b88555661e5aacfa16a00fd
JavaWithMahesh <ilearnjavaforever@gmail.com>
2026-01-17 07:24:24 -0500

    #1 Initial Commit for how to run/compile this project

---
+++ src/main/java/com/kafka/event/component/workflow/WorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.event.component.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/event/component/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.event.component.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
